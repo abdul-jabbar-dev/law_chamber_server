2f6e044ffc362248869fefbff2b9d54c003e6dbd
abdul-jabbar-dev <abduljabbardev@gmail.com>
2026-08-17 20:17:00 +0600

    sdfgsdfg

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,9 @@ const app: Express = express();
 const PORT = env.PORT;
 
 // Middleware 
-app.use(cors());
+app.use(cors({
+  origin: ["https://law-chamber-kappa.vercel.app", "https://law-chamber-kappa.vercel.app/*", "http://localhost:3000", "http://localhost:3000/*", "http://localhost:5000", "http://localhost:5000/*"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
